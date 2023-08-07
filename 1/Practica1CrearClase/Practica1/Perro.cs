3610f53b4728430ed0ac9832a46a83306d8d007b
Darío Docteur <94929890+DocteurDario@users.noreply.github.com>
2023-08-06 23:16:00 -0300

    Repaso

---
+++ 1/Practica1CrearClase/Practica1/Perro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Perro
+    {
+        //Funcion de C# que se llama Propiedad (es un metodo que engloba el getter y setter en el mismo bloque)
+        public string nombre { get; set; }
+        public string color { get; set; }
+        public string origen { get; set; }
+    }
+}
---
+++ 1/Practica1CrearClase/Practica1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Perro perro1 = new Perro(); // Ejemplo de como crear una variable de la clase perro
+            
+            perro1.color = "Blanco";
+            perro1.nombre = "Starck ";
+            perro1.origen = "de una casa, una amiga de la mamá del tío nico.";
+
+            Console.WriteLine("Nombre : ");
+            Console.WriteLine(perro1.nombre);
+            Console.WriteLine("Color :");
+            Console.WriteLine(perro1.color);
+            Console.WriteLine("Origen :");
+            Console.WriteLine(perro1.origen);
+
+            Perro perro2=new Perro();
+
+            perro2.color = "Negro";
+            perro2.nombre = "Sultan ";
+            perro2.origen = "de la calle.";
+
+            Console.WriteLine("Nombre : ");
+            Console.WriteLine(perro2.nombre);
+            Console.WriteLine("Color :");
+            Console.WriteLine(perro2.color);
+            Console.WriteLine("Origen :");
+            Console.WriteLine(perro2.origen);
+
+
+
+            Console.ReadKey();
+        }
+    }
+} 
